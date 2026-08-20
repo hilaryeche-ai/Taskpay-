af0da952bf211572b8dc1ba5d995267bf776c541
hilaryeche-ai <hilaryeche@gmail.com>
2026-08-20 17:53:53 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCKrP1u4qTV_qI_ClJt--aqG3Cj4DAvVS8",
+  authDomain: "taskpay-37e37.firebaseapp.com",
+  projectId: "taskpay-37e37",
+  storageBucket: "taskpay-37e37.firebasestorage.app",
+  messagingSenderId: "964721822071",
+  appId: "1:964721822071:web:6b40f663e80316fdab3972"
+};
